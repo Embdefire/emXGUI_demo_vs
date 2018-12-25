b242ed458f642fd654e7369db215fa7b956c3d37
flyleaf <flyleaf91@163.com>
2018-12-25 09:49:47 +0800

    创建副本准备更新clistmenu

---
+++ 综合demo_更新clist/GUI_App/GUI_DEMO/GUI_AppDef.h
@@ -0,0 +1,31 @@
+/**
+  *********************************************************************
+  * @file    GUI_AppDef.h
+  * @version V1.0
+  * @date    2018-xx-xx
+  * @brief   应用示例头文件
+  **********************************************************************
+  */
+#ifndef	__GUI_APPDEF_H__
+#define	__GUI_APPDEF_H__
+
+#define ID_SLIDE_WINDOW   1200
+
+#define HEAD_INFO_HEIGHT   90
+#define WM_MSG_FRAME_DOWN  (WM_USER+50)
+
+#define COLOR_DESKTOP_BACK_GROUND         215,61,50 //橙
+//#define COLOR_DESKTOP_BACK_GROUND         37,126,222	//蓝	
+//#define COLOR_DESKTOP_BACK_GROUND         100,202,2		//绿
+//#define COLOR_DESKTOP_BACK_GROUND         38,134,150		//深绿
+//#define COLOR_DESKTOP_BACK_GROUND         254,209,4		//深绿
+#define COLOR_INVALID                       165,160,160
+#define RGB888_COLOR_DESKTOP_BACK_GROUND   RGB888(215,61,50)
+#define RGB888_COLOR_INVALID                 RGB888(165,160,160)
+  //#define COLOR_BACK_GRUND RGB888(215,61,50)
+
+#define ICON_VIEWER_ID_PREV   0x1003
+#define ICON_VIEWER_ID_NEXT		0x1004
+
+
+#endif	/*__GUI_APPDEF_H__*/
